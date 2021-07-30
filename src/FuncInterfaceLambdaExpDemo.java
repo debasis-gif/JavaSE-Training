19150b9cee9bfb0e5f478a41961e4ea084aaa0ea
debasispal <debasis_pal_123@yahoo.co.uk>
2021-07-30 14:51:24 +0530

    Second commit to add pptx, Lambda exp demo

--- src/FuncInterfaceLambdaExpDemo.java
+++ src/FuncInterfaceLambdaExpDemo.java
@@ -9,22 +9,25 @@
 					   // when more than one method is mentioned in a Functional interface
 interface Abcd 
 {
-	void show();
+	//int x = 21;
+	void show(int x);
+	
 }
 
-public class FuncInterfaceLambdaExpDemo {
+public class FuncInterfaceLambdaExpDemo 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// Lambda expression is derived due to the fact that we are using a Functional Interface
 		// Functional interface has only 1 method
 		// Also the known things are Abcd Interface, known method show(), so we can eliminate the knowns
 		// new is not required as we cannot create an object of an Interface and also since the Anonymous class structure is also needed
 		// with Lambda expression () -> System.out.println("Inside Anonymous class to implement Interface...");
-		
-		Abcd obj = () -> System.out.println("Inside Anonymous class to implement Interface...");  
+	
+		Abcd obj = (x)-> System.out.println("Inside Anonymous class to implement Interface..."+x);  
 		// Creating an object of an Interface in one line
 		// This stuff works only with Functional interface (one method) and Java 8 onwards
-		obj.show();
+		obj.show(21);
 	}
-
 }
